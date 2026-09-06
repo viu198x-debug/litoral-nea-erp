46b4b4e7fdab6c025268af7e89bffb22e70b1f32
viu198x-debug <viu198x@gmail.com>
2026-09-06 10:34:30 -0300

    domain: register construction core records service

--- backend/src/records/records.module.ts
+++ backend/src/records/records.module.ts
@@ -3,9 +3,10 @@ import { RecordsController } from "./records.controller";
 import { RecordsService } from "./records.service";
 import { OperationalRecordsService } from "./operational-records.service";
 import { BusinessRecordsService } from "./business-records.service";
+import { ConstructionCoreRecordsService } from "./construction-core-records.service";
 
 @Module({
   controllers: [RecordsController],
-  providers: [RecordsService, OperationalRecordsService, BusinessRecordsService],
+  providers: [RecordsService, OperationalRecordsService, BusinessRecordsService, ConstructionCoreRecordsService],
 })
 export class RecordsModule {}
