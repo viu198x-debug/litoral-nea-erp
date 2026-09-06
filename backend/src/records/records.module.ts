38d5a55c69d93ea4879e75233d4f00d1897927c6
viu198x-debug <viu198x@gmail.com>
2026-09-06 10:09:51 -0300

    domain: register enterprise transactional record service

--- backend/src/records/records.module.ts
+++ backend/src/records/records.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { RecordsController } from "./records.controller";
 import { RecordsService } from "./records.service";
 import { OperationalRecordsService } from "./operational-records.service";
+import { BusinessRecordsService } from "./business-records.service";
 
 @Module({
   controllers: [RecordsController],
-  providers: [RecordsService, OperationalRecordsService],
+  providers: [RecordsService, OperationalRecordsService, BusinessRecordsService],
 })
 export class RecordsModule {}
