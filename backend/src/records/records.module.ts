0486183277a3ca089e2e9f5940282de655e0c2aa
viu198x-debug <viu198x@gmail.com>
2026-09-06 09:59:19 -0300

    domain: register operational records adapter

--- backend/src/records/records.module.ts
+++ backend/src/records/records.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { RecordsController } from "./records.controller";
 import { RecordsService } from "./records.service";
+import { OperationalRecordsService } from "./operational-records.service";
 
 @Module({
   controllers: [RecordsController],
-  providers: [RecordsService],
+  providers: [RecordsService, OperationalRecordsService],
 })
 export class RecordsModule {}
