5b765f3b054ed1d95ed4853de8d631ad9abcfafe
viu198x-debug <viu198x@gmail.com>
2026-09-12 00:16:53 -0300

    treasury: register overview records controller

--- backend/src/treasury/treasury.module.ts
+++ backend/src/treasury/treasury.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { TreasuryController } from "./treasury.controller";
+import { TreasuryOverviewRecordsController } from "./treasury-overview-records.controller";
 import { TreasuryRecordsController } from "./treasury-records.controller";
 import { TreasuryService } from "./treasury.service";
 
 @Module({
-  controllers: [TreasuryRecordsController, TreasuryController],
+  controllers: [TreasuryOverviewRecordsController, TreasuryRecordsController, TreasuryController],
   providers: [TreasuryService],
 })
 export class TreasuryModule {}
